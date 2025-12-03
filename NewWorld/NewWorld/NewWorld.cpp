9eb8982e3597ab9ac8660dc9419beda7b9d0cbf2
AODINGLVP <812206870@qq.com>
2025-12-03 10:33:58 +0000

    宽体正方形

--- NewWorld/NewWorld/NewWorld.cpp
+++ NewWorld/NewWorld/NewWorld.cpp
@@ -448,15 +448,45 @@ public:
 		Vec3 p6 = Vec3(1.0f, 1.0f, 1.0f);
 		Vec3 p7 = Vec3(-1.0f, 1.0f, 1.0f);
 
-		vertices.push_back(addVertex(p0, Vec3(0.0f, 0.0f, -1.0f), 0.0f, 1.0f));
-		vertices.push_back(addVertex(p1, Vec3(0.0f, 0.0f, -1.0f), 1.0f, 1.0f));
-		vertices.push_back(addVertex(p2, Vec3(0.0f, 0.0f, -1.0f), 1.0f, 0.0f));
-		vertices.push_back(addVertex(p3, Vec3(0.0f, 0.0f, -1.0f), 0.0f, 0.0f));
+		vertices.push_back( addVertex(p0, Vec3(0.0f, 0.0f, -1.0f), 0.0f, 1.0f));
+		vertices.push_back( addVertex(p1, Vec3(0.0f, 0.0f, -1.0f), 1.0f, 1.0f));
+		vertices.push_back( addVertex(p2, Vec3(0.0f, 0.0f, -1.0f), 1.0f, 0.0f));
+		vertices.push_back( addVertex(p3, Vec3(0.0f, 0.0f, -1.0f), 0.0f, 0.0f));
+		vertices.push_back( addVertex(p5, Vec3(0.0f, 0.0f, 1.0f), 0.0f, 1.0f));
+		vertices.push_back( addVertex(p4, Vec3(0.0f, 0.0f, 1.0f), 1.0f, 1.0f));
+		vertices.push_back( addVertex(p7, Vec3(0.0f, 0.0f, 1.0f), 1.0f, 0.0f));
+		vertices.push_back( addVertex(p6, Vec3(0.0f, 0.0f, 1.0f), 0.0f, 0.0f));
+		vertices.push_back( addVertex(p4, Vec3(-1.0f, 0.0f, 0.0f), 0.0f, 1.0f));
+		vertices.push_back( addVertex(p0, Vec3(-1.0f, 0.0f, 0.0f), 1.0f, 1.0f));
+		vertices.push_back( addVertex(p3, Vec3(-1.0f, 0.0f, 0.0f), 1.0f, 0.0f));
+		vertices.push_back( addVertex(p7, Vec3(-1.0f, 0.0f, 0.0f), 0.0f, 0.0f));
 
+		vertices.push_back( addVertex(p1, Vec3(1.0f, 0.0f, 0.0f), 0.0f, 1.0f));
+		vertices.push_back( addVertex(p5, Vec3(1.0f, 0.0f, 0.0f), 1.0f, 1.0f));
+		vertices.push_back( addVertex(p6, Vec3(1.0f, 0.0f, 0.0f), 1.0f, 0.0f));
+		vertices.push_back( addVertex(p2, Vec3(1.0f, 0.0f, 0.0f), 0.0f, 0.0f));
+		vertices.push_back( addVertex(p3, Vec3(0.0f, 1.0f, 0.0f), 0.0f, 1.0f));
+		vertices.push_back( addVertex(p2, Vec3(0.0f, 1.0f, 0.0f), 1.0f, 1.0f));
+		vertices.push_back( addVertex(p6, Vec3(0.0f, 1.0f, 0.0f), 1.0f, 0.0f));
+		vertices.push_back( addVertex(p7, Vec3(0.0f, 1.0f, 0.0f), 0.0f, 0.0f));
+		vertices.push_back( addVertex(p4, Vec3(0.0f, -1.0f, 0.0f), 0.0f, 1.0f));
+		vertices.push_back( addVertex(p5, Vec3(0.0f, -1.0f, 0.0f), 1.0f, 1.0f));
+		vertices.push_back( addVertex(p1, Vec3(0.0f, -1.0f, 0.0f), 1.0f, 0.0f));
+		vertices.push_back( addVertex(p0, Vec3(0.0f, -1.0f, 0.0f), 0.0f, 0.0f));
 
 		std::vector<unsigned int> indices;
 		indices.push_back(0); indices.push_back(1); indices.push_back(2);
 		indices.push_back(0); indices.push_back(2); indices.push_back(3);
+		indices.push_back(4); indices.push_back(5); indices.push_back(6);
+		indices.push_back(4); indices.push_back(6); indices.push_back(7);
+		indices.push_back(8); indices.push_back(9); indices.push_back(10);
+		indices.push_back(8); indices.push_back(10); indices.push_back(11);
+		indices.push_back(12); indices.push_back(13); indices.push_back(14);
+		indices.push_back(12); indices.push_back(14); indices.push_back(15);
+		indices.push_back(16); indices.push_back(17); indices.push_back(18);
+		indices.push_back(16); indices.push_back(18); indices.push_back(19);
+		indices.push_back(20); indices.push_back(21); indices.push_back(22);
+		indices.push_back(20); indices.push_back(22); indices.push_back(23);
 		
 		mesh.init(core, vertices, indices);
 
@@ -634,11 +664,11 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
 
 	Matrix prespection;
-	prespection = prespection.Perspective(M_PI / 4, 1920 / 1080, 0.1f, 100.0f);
+	prespection = prespection.Perspective(M_PI / 4, 1920 / 1080, 0.3f, 100.0f);
 
 	ConstantBuffer3 constBufferCPU3;
 	
-	constBufferCPU3.VP= prespection.Perspective(M_PI / 4, 1920 / 1080, 0.1f, 100.0f);
+	constBufferCPU3.VP= prespection.Perspective(M_PI / 4, 1920 / 1080, 0.3f, 100.0f);
 	
 	GamesEngineeringBase::Timer timer;
 	
